860ec8bc7bc813742d4ad484d320f4414bfacb14
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2024-06-22 19:31:07 -0400

    week2

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -27,33 +27,34 @@ const viewData = async ()=>{
     for(let i of data){
         let div = document.createElement('div');
         div.className = "card";
+        
+        let h2 = document.createElement('h2');
+        h2.innerHTML = i.name;
         let img = document.createElement('img');
         img.src = i.icon;
         
         let ul = document.createElement('ul');
         
-        let li_1 = document.createElement('li');
-        li_1.innerHTML = i.name;
-
-        let li_2 = document.createElement('li');
-        li_2.innerHTML = i.address;
-
-        let li_3 = document.createElement('li');
-        li_3.innerHTML = i.phone_number;
-
-        let li_4 = document.createElement('li');
-        li_4.innerHTML = i.website_url;
-
        
 
-        let li_5 = document.createElement('li');
-        li_5.innerHTML = i.membership_level;
+        let li_1 = document.createElement('li');
+        li_1.innerHTML = `<span class="lable">EMAIL:</span> ${i.email}`;
 
-        let li_6 = document.createElement('li');
-        li_6.innerHTML = i.established;
+        let li_2 = document.createElement('li');
+        li_2.innerHTML = `<span class="lable">PHONE:</span> ${i.phone_number}`;
+
+        let li_3 = document.createElement('li');
+        li_3.innerHTML = `<span class="lable">URL:</span> ${i.website_url}`;
+
+        let li_4 = document.createElement('li');
+        li_4.innerHTML = i.membership_level;
+
+        let li_5 = document.createElement('li');
+        li_5.innerHTML = i.established;
 
        
         block.appendChild(div);
+        div.appendChild(h2)
         div.appendChild(img);
         div.appendChild(ul);
         ul.appendChild(li_1);
@@ -61,7 +62,7 @@ const viewData = async ()=>{
         ul.appendChild(li_3);
         ul.appendChild(li_4);
         ul.appendChild(li_5);
-        ul.appendChild(li_6);
+        
         
         console.log(i.name);
     }
---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,8 @@
+
+let currentYear = new Date().getFullYear();
+let date_element = document.getElementById('currentyear');
+date_element.innerHTML = currentYear;
+
+let lastModified =  document.lastModified;
+let lastModified_element = document.getElementById('lastModified');
+lastModified_element.innerHTML = lastModified;
